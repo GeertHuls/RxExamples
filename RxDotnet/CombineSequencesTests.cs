1a387c66fb45d7e436893e221c1b7a7593bd3b65
Geert Huls <geert.huls82@gmail.com>
2017-05-27 22:51:56 +0200

    Amb operator test

--- RxDotnet/CombineSequencesTests.cs
+++ RxDotnet/CombineSequencesTests.cs
@@ -19,7 +19,25 @@ namespace RxDotnet
 			observable.Subscribe(new LoggerObserver<long>());
 
 			await Task.Delay(8000);
+		}
 
+		/// <summary>
+		/// The amb operator only pushes those values of the sequences
+		/// that is first to push values.
+		/// </summary>
+		/// <returns></returns>
+		[Fact]
+		public async Task AmbigiousTests()
+		{
+			var observable1 = Observable.Interval(TimeSpan.FromSeconds(1));
+			var observable2 = Observable.Interval(TimeSpan.FromSeconds(1))
+				.Delay(TimeSpan.FromSeconds(2));
+
+			var observable = observable1.Amb(observable2);
+
+			observable.Subscribe(new LoggerObserver<long>());
+
+			await Task.Delay(8000);
 		}
 	}
 }
