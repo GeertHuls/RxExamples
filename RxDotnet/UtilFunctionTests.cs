20819f48a406b5f13104624eafc633d707fd69cc
Geert Huls <geert.huls82@gmail.com>
2017-06-01 21:57:52 +0200

    Throttle example

--- RxDotnet/UtilFunctionTests.cs
+++ RxDotnet/UtilFunctionTests.cs
@@ -32,5 +32,22 @@ namespace RxDotnet
 
 			await Task.Delay(5000);
 		}
+
+		/// <summary>
+		/// Throttle is used to reduce the rate of observables
+		/// at the cost of losing intermediate values.
+		/// Usefull for working with sensor data.
+		/// </summary>
+		/// <returns></returns>
+		[Fact]
+		public async Task ThrottleFunctionTest()
+		{
+			var observable = new[] { 1, 2, 3, 4, 5 }.ToObservable();
+
+			observable.Throttle(TimeSpan.FromSeconds(1))
+				.Subscribe(new LoggerObserver<int>());
+
+			await Task.Delay(5000);
+		}
 	}
 }
